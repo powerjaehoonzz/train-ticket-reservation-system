7fd2c3e2058993c5f1b1db99c31572122f915967
powerjaehoonzz <powerjaehoon@gmail.com>
2026-08-27 23:12:06 +0900

    chore: 데이터베이스 연결 설정

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -2,8 +2,11 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from sqlmodel import SQLModel
 
 from alembic import context
+
+from models import User, Station, Train, Seat, TrainSchedule, Reservation
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +21,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -64,9 +67,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
---
+++ backend/models/__init__.py
@@ -0,0 +1,6 @@
+from models.reservation import Reservation
+from models.seat import Seat
+from models.station import Station
+from models.train import Train
+from models.train_schedule import TrainSchedule
+from models.user import User
